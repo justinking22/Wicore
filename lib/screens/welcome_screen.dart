bc3dea8b4abcbe0ffca3354974e58b1c2cc2deab
Justin Whiter-Apps <justinshox0907@gmail.com>
2025-08-06 21:48:22 +0900

    flow issue fixed

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -38,7 +38,7 @@ class WelcomeScreen extends StatelessWidget {
               text: '다음',
               onPressed: () {
                 // Navigate to the first step of the registration process
-                context.push('/terms-of-use');
+                context.push('/terms-and-conditions');
               },
             ),
             const SizedBox(height: 32),
